1108f1a527ecd013206c7adc3344d0c3b534bd25
celticg <r.bodnar@setonhill.edu>
2019-08-13 11:19:08 -0400

    split month Queries work, but won't order properly; trying SQL method

--- app/src/main/java/com/example/biweeklybudget/BillDao.java
+++ app/src/main/java/com/example/biweeklybudget/BillDao.java
@@ -32,9 +32,11 @@ public interface BillDao {
     @Query("SELECT * from bill_table WHERE due >= :today AND due <= :fin ORDER BY due ASC")
     LiveData<List<Bill>> getAfter(int today, int fin);
 
-    @Query("SELECT * from bill_table WHERE due <= :today AND due >= 1 UNION SELECT * FROM bill_table WHERE due >= :fin AND due <= 28 ORDER BY due ASC")
+    @Query("SELECT * from bill_table WHERE due >= :today AND due <= 28 UNION SELECT * FROM bill_table WHERE due <= :fin AND due >= 1 ORDER BY due ASC")
     LiveData<List<Bill>> getAfterCrossMonths(int today, int fin);
 
     @Query("SELECT COUNT(id) from bill_table")
     LiveData<Integer> getBillCount();
+
+    //@Query("SELECT * from bill_table WHERE due >= :today")
 }
